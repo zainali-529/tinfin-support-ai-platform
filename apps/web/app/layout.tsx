7ecc32dc645a04541dbcdced4dbc4ae6cda97a92
Zain Ali <zainop001@gmail.com>
2026-04-06 17:53:31 +0500

    feat: initial commit

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,30 @@
+import { Geist, Geist_Mono, Raleway } from "next/font/google"
+
+import "@workspace/ui/globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@workspace/ui/lib/utils";
+
+const raleway = Raleway({subsets:['latin'],variable:'--font-sans'})
+
+const fontMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn("antialiased", fontMono.variable, "font-sans", raleway.variable)}
+    >
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  )
+}
